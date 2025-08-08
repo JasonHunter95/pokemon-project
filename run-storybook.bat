77a1734305b088527d8916ca6cc5dc85893249fe
Jason Hunter <jahu8701@colorado.edu>
2025-08-08 17:40:21 -0600

    small update to storybook server structure

--- run-storybook.bat
+++ run-storybook.bat
@@ -1,8 +1,8 @@
 @echo off
 REM This script starts both the React frontend and Storybook servers for the Pokemon project.
 REM Run this script from the root of your project directory.
-echo Starting the Pokemon React frontend...
-start "React Frontend" cmd /k "npm start"
+cd frontend
+start "Installing Dependencies for local Storybook development." cmd /k "npm install"
 echo Starting Storybook...
 start "Storybook" cmd /k "npm run storybook"
-echo Both servers are starting in separate windows.
+echo Storybook is now running in the browser at http://localhost:6006
